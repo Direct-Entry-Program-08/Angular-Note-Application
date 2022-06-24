9c59f0e1722b2a2b02f68bcbae97f39fb5a1a946
Thilina-Upendra <upendravass1996@gmail.com>
2022-06-24 20:29:25 +0530

    Get all the notes when app Initialize

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NoteService} from "./service/note.service";
+import {Note} from "./dto/note";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'note-angular-fornt-end';
+export class AppComponent implements OnInit{
+
+  notes:Array<Note> = [];
+
+  constructor(private noteService: NoteService) {
+  }
+
+  ngOnInit(): void {
+    this.noteService.getAllNotes().subscribe({
+      next:notes => this.notes = notes
+    });
+  }
+
 }
